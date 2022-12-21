67521fb7fc087c9f1b8e7479424b12b98bc1c554
yantheLousada <yanthe96@hotmail.com>
2022-12-21 09:21:39 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById('root'),
+);
